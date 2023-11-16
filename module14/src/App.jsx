e51a0baf55b8ce3c74a178e7f475dcb23bca6071
ram-elife <ramrachai@elifetransfer.com>
2023-11-16 19:44:49 +0600

    navbar is nicely added and working

--- module14/src/App.jsx
+++ module14/src/App.jsx
@@ -1,21 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Home, Projects, Services, Team, Testimonials } from "./pages";
-import Header from "./components/Header/Header"
-import { Header2 } from "./components/Header/Header2";
+import Navbar from "./components/Header/Navbar3"
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
-      <Header2 />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/team" element={<Team />} />
-        <Route path="/testimonial" element={<Testimonials />} />
-      </Routes>
+      <Navbar />
+      <div className="pages">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/testimonial" element={<Testimonials />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ module14/src/components/Header/Navbar3.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+import { FaBars, FaXmark } from "react-icons/fa6";
+import logoImg from "../../assets/common/DesignAGENCY.png"
+
+
+function NavBar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
+    return (
+        <>
+            <nav className="navbar">
+                <div className="nav-container">
+                    <NavLink to="/" className="nav-logo">
+                        <img src={logoImg} alt="logo" />
+                    </NavLink>
+
+                    <ul className={click ? "nav-menu active" : "nav-menu"}>
+                        <li className="nav-item">
+                            <NavLink to="/" className="nav-links">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/team" className="nav-links">Team</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/services" className="nav-links">Service</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/projects" className="nav-links">Projects</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/testimonial" className="nav-links">Testimonials</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <button className="outlined">Login</button>
+                        </li>
+                        <li className="nav-item">
+                            <button>Register</button>
+                        </li>
+                    </ul>
+                    <div className="nav-icon" onClick={handleClick}>
+                        {click ? (
+                            <span className="icon">
+                                <FaXmark />
+                            </span>
+                        ) : (
+                            <span className="icon">
+                                <FaBars />
+                            </span>
+                        )}
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default NavBar;
