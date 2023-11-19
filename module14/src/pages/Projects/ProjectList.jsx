74755f6974d8b6fd9462b96ed9675bf270fb7654
ram-elife <ramrachai@elifetransfer.com>
2023-11-19 18:51:06 +0600

    removed unused import

--- module14/src/pages/Projects/ProjectList.jsx
+++ module14/src/pages/Projects/ProjectList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import ProjectItem from './ProjectItem'
 import PropTypes from 'prop-types'
 import styled from '@emotion/styled';
